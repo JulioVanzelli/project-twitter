db05579f3c2bf8109745d947de36a35149c67c73
JulioVanzelli <julio.vanzelli@gmail.com>
2019-01-16 00:53:26 -0200

    Removal of unnecessary comments.

--- twitter small analytics/src/twitter/Main.java
+++ twitter small analytics/src/twitter/Main.java
@@ -7,17 +7,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		TwitterClient twitterClient = new TwitterClient(
-				"Y4mDeKYjHHPWC3iurthtAtPIW",
-				"4yNdfPmUNaIrGnVN0XJNCffWPfzuC3sDHIlykXtqgUZdbZHB6b",
-				"3405826665-cEAJizD5sefVqfkmnf6gg1RN49OmVLsSZAWdSuW", 
-				"VrZQuwUAvbV5RMjkUvFLTYUO86gRtadqyGm8wyhLFuD2w"
+				"",
+				"",
+				"", 
+				""
 			);
 
 		TwitterService twitterService = new TwitterService(twitterClient);
-		
-		//twitterService.updateStatus("@michelpf finalizei a aplicação.");
-		
-		//twitterService.sendDirectMessage("neverislate", "testando minha aplicação Novamente.");
 
 		// hashtag e tweets da última semana
 		ArrayList<TweetsPerDay> dataTweets = twitterService.searchDetailedTweetsData(
@@ -35,9 +31,7 @@ public class Main {
 			
 			total.addAll(dataTweets.get(i).getListTweets());
 		}
-		
-		// 08GhostFace foi o primeiro autor e zeroturnaround último autor.
-		
+
 		System.out.println("==================================================");
 		String[] aux = TweetHelper.getFirstAndLastAuthor(total);
 		System.out.println( aux[1] != null ? aux[0] + " foi o primeiro autor e "+ aux[1] + " último autor." : "Sem autor." );
@@ -45,8 +39,7 @@ public class Main {
 		aux = TweetHelper.getFirstAndLastTweetDate(total);
 	   	System.out.println( aux[1] != null ? aux[0] + " foi o primeiro tweet e "+ aux[1] + " último tweet." : "Sem tweet." );
 		System.out.println("==================================================");
-		
-		//twitterService.updateStatus("@michelpf finalizei a aplicação.");
+
 	}
 
 }
